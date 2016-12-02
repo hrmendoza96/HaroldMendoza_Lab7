8584ff142c164c81ebf66deae13cb47b70ccc417
Harold Mendoza <hrmendoza96@unitec.edu>
2016-12-02 13:28:58 -0600

    Commit 1

---
+++ Main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void menu();
+
+int main(int argc, char const *argv[]) {
+  int opcion=0;
+  do {
+    menu();
+    cin >> opcion;
+    if(opcion==1){
+
+    }
+    if(opcion==2){
+
+    }
+    if(opcion==3){
+
+    }
+
+    if(opcion==4){
+      cout << "Hasta Luego" << endl;
+
+    }
+
+  } while(opcion!=4);
+
+  return 0;
+}
+
+void menu(){
+    cout << "Programa Administrador de Usuarios" << endl;
+    cout << "1) : " << endl;
+    cout << "2) : " << endl;
+    cout << "3) : " << endl;
+    cout << "4) : " << endl;
+
+    cout << "Ingrese su respuesta: " << endl;
+}
